6530fa5ba12cb2e7ca7b3759db1696476b4e882a
Aqeel-Codes <162784473+Aqeel-Codes@users.noreply.github.com>
2024-03-09 16:07:16 +0500

    Add files via upload

---
+++ python operators.py
@@ -0,0 +1,87 @@
+# print in python
+print("Wellcome to python world")
+
+# addition in python
+print("2 + 3 =",2+3)
+
+# subtraction in python
+print("3 - 1 =",3-1)
+
+# mulplication in python
+print("2 x 3 =",2*3)
+
+# division in python
+print("10 / 5 =",10/5)
+
+# modulus in python 
+print("10 % 5 =",10%5)
+
+# increment in python
+x=1
+x+=1
+print("if the value of x is 1 then the incremental value of x is :",x)
+
+# decrement in python
+x=2
+x-=1
+print("if the value of x is 2 then the decremental value of x is :",x)
+
+# == comparison operator in python
+a=2
+print("The comparison of a==2 is :",a==2)
+
+# equal to operator in python
+a=2
+print("The answer of a=2 is :",a)
+
+# != comparison operator in python
+a=2
+print("The comparison of a!=2 is :",a!=2)
+
+# > operator in python
+a=2
+b=3
+print("The value is a>b is: ",a>b)
+
+# >= operator in python
+a=2
+b=2
+print("The value is a>=b is: ",a>=b)
+
+# <= operator in python
+a=2
+b=2
+print("The value is a<=b is: ",a<=b)
+
+# // in python 
+a=45
+b=6
+print("The value is a//b is: ",a//b)
+
+# & logial operator in python
+a=45
+b=6
+print("The value is a&b is: ",a&b)
+
+# and logial operator in python
+a = 10
+print("The value is a and a is: ",a>4 and a>3)
+
+# or logial operator in python
+a = 10
+print("The value is a or a is: ",a>4 or a>3)
+
+# not logial operator in python
+a = 10
+print("The value is a not is: ",not(a>4 and a>3))
+
+
+
+
+
+
+
+
+
+
+
