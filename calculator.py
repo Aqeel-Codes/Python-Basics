cd35ed65ea3ed87e33faaf1a763b850bc9934617
Aqeel-Codes <162784473+Aqeel-Codes@users.noreply.github.com>
2024-03-10 14:06:46 +0500

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -1,21 +1,32 @@
 # Calculator in python
-num1 = int(input("Enter the first integer :  "))
-num2 = int(input("Enter the second integer:  "))
 oprtr = input("Enter the operator sign (+, -, * ,/ ,// ,% ,^2) which one you want to perform : ")
 
 if oprtr == '+':
+   num1 = int(input("Enter the first integer :  "))
+   num2 = int(input("Enter the second integer:  "))
    print("The result of ",num1," + ",num2," is : ",num1+num2)
 elif oprtr == '-':
+   num1 = int(input("Enter the first integer :  "))
+   num2 = int(input("Enter the second integer:  "))
    print("The result of ",num1," - ",num2," is : ",num1-num2)
 elif oprtr == '*':
+   num1 = int(input("Enter the first integer :  "))
+   num2 = int(input("Enter the second integer:  "))
    print("The result of ",num1," x ",num2," is : ",num1*num2)
 elif oprtr == '/':
+   num1 = int(input("Enter the first integer :  "))
+   num2 = int(input("Enter the second integer:  "))
    print("The result of ",num1," / ",num2," is : ",num1/num2)
 elif oprtr == '//':
+   num1 = int(input("Enter the first integer :  "))
+   num2 = int(input("Enter the second integer:  "))
    print("The result of ",num1," // ",num2," is : ",num1//num2)
 elif oprtr == '%':
+   num1 = int(input("Enter the first integer :  "))
+   num2 = int(input("Enter the second integer:  "))
    print("The result of ",num1," % ",num2,"is : ",num1%num2)
 elif oprtr == '^2':
-   print("The square of ",num1," is : ",num1*num1," \nThe square of ",num2," is : ",num2*num2)
+   num1 = int(input("Enter the first integer :  "))
+   print("The square of ",num1," is : ",num1*num1)
 else:
    print("You put wrong operator.")
