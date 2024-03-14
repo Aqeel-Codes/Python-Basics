e2324088662142e28a916d1b8bf20a831e7b3d9c
Aqeel-Codes <162784473+Aqeel-Codes@users.noreply.github.com>
2024-03-14 06:40:27 +0500

    Add files via upload

--- temperature_converter.py
+++ temperature_converter.py
@@ -1,18 +1,15 @@
 # converting temperature from celcius to Fahrenheit and Fahrenheit to celcius
-ask = input("Enter the temerature unit in which you want to convert your like f or c : ")
-def c_to_f(temp):
-	cnvrtr = (9/5 * temp) + 32
-	r = round(cnvrtr,2)
-	return r
-def f_to_c(temp1):
-	cnvrtr = (temp1 - 32) * 5/9
-	r = round(cnvrtr,2)
-	return r
-if ask == 'c':
-	temp = float(input("Enter the temperature in celcius : "))
-	print("Celcius temperature in fahrenheit is : ",c_to_f(temp),"°F")
-else:
-	temp1 = float(input("Enter the temperature in fahrenheit : "))
-	print("Fahrenheit temperature in celcius is : ",f_to_c(temp1),"°C")
+temp = float(input("Enter the temperature in celcius : "))
+unit = input("Enter the unit of temperature : ")
+def temp_converter(temp,unit):
+	if unit == 'c':
+		cnvrtr = (9/5 * temp) + 32
+		r = round(cnvrtr,2)
+		return (f"After converting celcius to fahrenheit is : {r}F")
+	else:
+		cnvrtr = (temp - 32) * 5/9
+		r = round(cnvrtr,2)
+		return (f"After converting fahrenheit to celcius is : {r}C")
 
 
+print(temp_converter(temp,unit))
